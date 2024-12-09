f25882b58306162671267584fd0829b9b5539926
AraratDavtyan <Davtyanaro080@gmail.com>
2024-12-10 00:25:53 +0400

    Create Interpolation Search.py

---
+++ Interpolation Search.py
@@ -0,0 +1,11 @@
+def interpolation_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high and target >= arr[low] and target <= arr[high]:
+        pos = low + ((high - low) * (target - arr[low])) // (arr[high] - arr[low])
+        if arr[pos] == target:
+            return pos
+        if arr[pos] < target:
+            low = pos + 1
+        else:
+            high = pos - 1
+    return -1
